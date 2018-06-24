3c9fe18260533f2824c3c30d51177914cda03b6f
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-06-24 12:18:28 -0700

    mostly working mvc structure

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -3,84 +3,84 @@ import './login.css';
 
 const Login = () => {
 	return (
-<div class="container">
-    	<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<div class="panel panel-login">
-					<div class="panel-heading">
-						<div class="row">
-							<div class="col-xs-6">
-								<a href="#" class="active" id="login-form-link">Login</a>
+		<div className="container">
+			<div className="row">
+				<div className="col-md-6 col-md-offset-3">
+					<div className="panel panel-login">
+						<div className="panel-heading">
+							<div className="row">
+								<div className="col-xs-6">
+									<a href="#" className="active" id="login-form-link">Login</a>
+								</div>
+								<div className="col-xs-6">
+									<a href="#" id="register-form-link">Register</a>
+								</div>
 							</div>
-							<div class="col-xs-6">
-								<a href="#" id="register-form-link">Register</a>
-							</div>
+							<hr />
 						</div>
-						<hr/>
-					</div>		
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-lg-12">
+						<div className="panel-body">
+							<div className="row">
+								<div className="col-lg-12">
 									<form id="login-form" action="#" method="post" role="form" styles={{ display: 'block' }}>
-									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value=""/>
-									</div>
-									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password"/>
-									</div>
-									<div class="form-group text-center">
-										<input type="checkbox" tabindex="3" class="" name="remember" id="remember"/>
-										<label for="remember"> Remember Me</label>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In"/>
+										<div className="form-group">
+											<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" value="" />
+										</div>
+										<div className="form-group">
+											<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
+										</div>
+										<div className="form-group text-center">
+											<input type="checkbox" tabIndex="3" className="" name="remember" id="remember" />
+											<label htmlFor="remember"> Remember Me</label>
+										</div>
+										<div className="form-group">
+											<div className="row">
+												<div className="col-sm-6 col-sm-offset-3">
+													<input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Log In" />
 												</div>
-												<div class="row">
-												<div class="fb-login-button" data-width="375" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true">
-												</div>	
-											</div>
-											</div>	
-										</div>	
-									<div class="form-group">
-										<div class="row">
-											<div class="col-lg-12">
-												<div class="text-center">
-													<a href="#" tabindex="5" class="forgot-password">Forgot Password?</a>
+												<div className="row">
+													<div className="fb-login-button" data-width="375" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true">
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
-								</form>
+										<div className="form-group">
+											<div className="row">
+												<div className="col-lg-12">
+													<div className="text-center">
+														<a href="#" tabIndex="5" className="forgot-password">Forgot Password?</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</form>
 									<form id="register-form" action="#" method="post" role="form" styles={{ display: 'none' }}>
-									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value=""/>
-									</div>
-									<div class="form-group">
-										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value=""/>
-									</div>
-									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password"/>
-									</div>
-									<div class="form-group">
-										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password"/>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now"/>
+										<div className="form-group">
+											<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" value="" />
+										</div>
+										<div className="form-group">
+											<input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" value="" />
+										</div>
+										<div className="form-group">
+											<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
+										</div>
+										<div className="form-group">
+											<input type="password" name="confirm-password" id="confirm-password" tabIndex="2" className="form-control" placeholder="Confirm Password" />
+										</div>
+										<div className="form-group">
+											<div className="row">
+												<div className="col-sm-6 col-sm-offset-3">
+													<input type="submit" name="register-submit" id="register-submit" tabIndex="4" className="form-control btn btn-register" value="Register Now" />
+												</div>
 											</div>
 										</div>
-									</div>
-								</form>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	)
 };
 
