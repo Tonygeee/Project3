f9c1079c5d7b7217012f6dc42922c2a559248728
Maureen Bockrath <maureenbockrath@Maureens-Air.localdomain>
2018-06-28 20:29:37 -0700

    api start

--- routes/api/events.js
+++ routes/api/events.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 router.get("/:zipCode", (req, res) => {
     axios
-        .get("https://app.ticketmaster.com/discovery/v2/events.json", { params: req.query })
+        .get("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=NyUgIOdhF4S6fXn0BFIY7Afii13kPKnc", { params: req.query })
         .then(({ data: { results } }) => res.json(results))
         .catch(err => res.status(422).json(err));
 });
