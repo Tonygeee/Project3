d8e8d24765d3fde270d6612c3e6dc759a2e702b8
Tony Gonzalez <Tony@AntoniosMacBook.localdomain>
2018-07-03 21:02:27 -0700

    Ticketmaster widget added to events card

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,15 +9,16 @@ export default {
   getProfile: function (id) {
     return axios.get("/api/main/" + id);
   },
-  // Deletes the profile with the given id
-  deleteProfile: function (id) {
-    return axios.delete("/api/main/" + id);
-  },
-  // Saves a profile to the database
-  saveProfile: function (profileData) {
-    return axios.post("/api/main", profileData);
-  },
-  getEvents: function (query) {
-    return axios.get("api/search", { params: { q: query } });
+    // Deletes the profile with the given id
+    deleteProfile: function (id) {
+      return axios.delete("/api/main/" + id);
+    },
+    // Saves a profile to the database
+    saveProfile: function (profileData) {
+      return axios.post("/api/main", profileData);
+    },
+    getEvents: function (query) {
+      return axios.get("api/search", { params: { q: query } });
+    }
   }
-};
+
