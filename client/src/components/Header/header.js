d672a01762f9cea4d4ff900e4e007c65c9ec6ea2
Vanessa Gomez <vanegogogo@gmail.com>
2018-07-11 20:53:59 -0700

    signoutbutton

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -9,7 +9,7 @@ function Header() {
                 <div id="searching">
                     <form className="form-inline my-2 my-lg-0">
                         <div id="searching">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button id="signOutBtn" type="button" class="btn btn-light">Sign Out</button>
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
                         </div>
                     </form>
