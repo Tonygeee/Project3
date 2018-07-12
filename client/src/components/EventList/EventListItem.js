8a189785d2fef49aa89e1159f75406cf32b35f7d
Vanessa Gomez <vanegogogo@gmail.com>
2018-07-11 20:42:45 -0700

    chat and events

--- client/src/components/EventList/EventListItem.js
+++ client/src/components/EventList/EventListItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './eventlistitem.css';
+
 
 export const EventListItem = props => (
 
-    <li>
-        <a href={props.url}><h3>{props.title}</h3></a>
-        <h4>{props.venue}</h4>
-        <p>{props.eventDate}</p>
-        <p>{props.eventTime}</p>
-        <button onClick={props.onClick} data-index={props.index}>Intrested</button>
-    </li>
+    <div id="events">
+        <li>
+            <a href={props.url}><h3>{props.title}</h3></a>
+            <h4>{props.venue}</h4>
+            <p>{props.eventDate}</p>
+            <p>{props.eventTime}</p>
+            <button onClick={props.onClick} data-index={props.index}>Intrested</button>
+        </li>
+    </div>
 );
